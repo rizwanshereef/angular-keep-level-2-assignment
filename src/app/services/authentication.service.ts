c74cd59132677ae7fca47bb800fc5451dad2e62f
rsrizwan77 <rsrizwan77@gmail.com>
2020-10-11 22:35:20 +0530

    Adding code

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthenticationService {
+  private authUrl: string;
+  constructor(private httpClient: HttpClient) {
+    this.authUrl = 'http://localhost:3000/auth/v1/';
+  }
 
-  constructor() {
+
+  authenticateUser(data:any) {
+    return this.httpClient.post(this.authUrl, data);
 
   }
 
-  authenticateUser(data) {
-
-  }
-
-  setBearerToken(token) {
-
+  setBearerToken(token:string) {
+    localStorage.setItem('bearerToken', token);
   }
 
   getBearerToken() {
+    return localStorage.getItem('bearerToken');
 
   }
 
-  isUserAuthenticated(token): Promise<boolean> {
-
+  isUserAuthenticated(token): Promise<any> {
+    return this.httpClient.post(this.authUrl + 'isAuthenticated', {}, {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+    }).pipe(map(reponse => reponse['isAuthenticated'])).toPromise();
   }
 }
